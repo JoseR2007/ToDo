33984f8d61603c89b00ebaa753ee1b59481b3cc7
Jose Rafael <joserafaeltineog@gmail.com>
2024-03-01 11:51:36 -0400

    Implementacion basica de la funcionalidad

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const btnAgregar = document.getElementById('agregar');
 const tareaInput = document.getElementById('tarea');
 const tabla = document.getElementById('tareas');
+
+btnAgregar.addEventListener("click", ()=>{
+  const article = document.createElement('article');
+  const h2 = document.createElement('h2');
+    
+  h2.append(tareaInput.value);
+  article.appendChild(h2);
+  tabla.appendChild(article);
+})
